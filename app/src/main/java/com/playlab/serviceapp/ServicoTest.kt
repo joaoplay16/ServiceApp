bc924923913ff2256a149b01d7cb15cb0212e988
João Pedro <joaoplay16@gmail.com>
2022-08-26 20:20:57 -0300

    Worker class added

--- app/src/main/java/com/playlab/serviceapp/ServicoTest.kt
+++ app/src/main/java/com/playlab/serviceapp/ServicoTest.kt
@@ -3,6 +3,7 @@ package com.playlab.serviceapp
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 class ServicoTest : Service() {
 
@@ -12,5 +13,39 @@ class ServicoTest : Service() {
 
     override fun onCreate() {
         super.onCreate()
+        Log.i("Script", "onCreate()")
+
     }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i("Script", "onStartCommand()")
+
+        val w = Worker(startId)
+        w.start()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    inner class Worker() : Thread() {
+        var count: Int = 0
+        var startId: Int = 0
+        var ativo: Boolean = true
+
+        constructor(startId: Int) : this() {
+            this.startId = startId
+        }
+
+        override fun run (){
+            while (ativo && count < 10){
+                try {
+                    Thread.sleep(1000)
+                }catch (e: InterruptedException){
+                    e.printStackTrace()
+                }
+                count++
+                Log.i("Script", "COUNT: $count")
+            }
+            stopSelf(startId)
+        }
+    }
+
 }
